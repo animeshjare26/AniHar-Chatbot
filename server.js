6ad079926700e52f4db4fcfd50a27af8159574ae
animeshjare <127379166+animeshjare26@users.noreply.github.com>
2024-10-30 23:40:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,22 +1,5 @@
 
 
-// const { GoogleGenerativeAI } = require("@google/generative-ai");
-
-// // Access your API key as an environment variable (see "Set up your API key" above)
-// const genAI = new GoogleGenerativeAI("AIzaSyCsLzQGsphyT3BnYxEVZ0_8nqYgQVkXOm0");
-
-// const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash"});
-
-// async function run() {
-//     const prompt = "Write a story about an AI and magic"
-  
-//     const result = await model.generateContent(prompt);
-//     const response = await result.response;
-//     const text = response.text();
-//     console.log(text);
-//   }
-  
-//   run();
 
 const express = require('express');
 const bodyParser = require('body-parser');
@@ -26,7 +9,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 // Access your API key as an environment variable
-const genAI = new GoogleGenerativeAI('AIzaSyAso0f_KZEEoLZl2zTzwy3uKhniumT56YY');
+const genAI = new GoogleGenerativeAI('xxxxxxxxxxxxxxxxxxxxx');
 const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
 
 // Middleware
